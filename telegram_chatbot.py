02d4e07f0068135718ca02b6041a9cd8f90e8a44
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2024-08-14 01:40:07 +0800

    Update telegram_chatbot.py

--- telegram_chatbot.py
+++ telegram_chatbot.py
@@ -9,60 +9,19 @@
 
 # Chatbase custom GPT model API documentation: https://docs.chatbase.co/docs/create-a-chatbot 
 
-
-# ///////////////////////////////////////////////////////////////////////////
-
-
-# Instructions to integrating your own trained Chatbase custom GPT model API to this Telegram Bot code:
-# Simply replace your Chatbase custom GPT model API key (e.g. 3c255g98b-c5fe-4324a7-343eb-f5d4353536e (this is a
-# fake API key)) with the '<API KEY HERE>'! If you are not sure how to find your Chatbase custom GPT model API 
-# key, see the instructions below under the section, 'How to find the Chatbase custom GPT model API key'
-
-# Also replace your Chatbase custom GPT model chatbot ID (e.g. -f0oTRCOFh5frn2dPpi (this is a
-# fake API key)) with the '<CHATBOT ID HERE>'! If you are not sure how to find your Chatbase custom GPT model chatbot 
-# ID, see the instructions below under the section, 'How to find the Chatbase custom GPT model chatbase ID'
-
-
-# ///////////////////////////////////////////////////////////////////////////
-
-
 import requests
 import json
 import telegram.ext     # (Note that the version of the telegram Python library is version 13.3)
-import pymysql
+from firebase_admin import db
+import firebase_admin
 
-
-# Chatbase custom GPT model API, but with Python's 'requests' and json' libraries as substitude, because
-# Chatbase custom GPT model API does not have their own Python library code unlike OpenAI GPT model API 
 url = 'https://www.chatbase.co/api/v1/chat'
 
-
-# How to find the Chatbase custom GPT model API key:
-# 1. Log in to Chatbase: Go to the Chatbase website and log in with your Google account credentials.
-# 2. Navigate to Your Account: After logging in, navigate to the dashboard where you can manage your 
-#    chatbots, settings and view analytics.
-# 3. Access API Settings: In your Chatbase dashboard, look for settings options under the "Settings" 
-#    menu.
-# 4. Find Your API Key: Within the developer or API settings, you should find your API key, labeled as 
-#    "API Key".
 headers = {
     'Authorization': 'Bearer 3c7b798b-c5fe-41a7-bdeb-f5d0b6f8536e',
     'Content-Type': 'application/json'
 }
 
-# How to find the Chatbase custom GPT model chatbot ID:
-# 1. Log in to Chatbase: Go to the Chatbase website and log in with your Google account credentials.
-# 2. Navigate to the Dashboard:After logging in, navigate to the dashboard where you can manage your 
-#    chatbots, settings and view analytics.
-# 3. Select Your chatbot: From the dashboard, locate the chatbot for which you want to find the chatbot ID. 
-#    You might see a list of chatbots if you have more than one. Click on the chatbot's name or the 
-#    associated link to access its details.
-# 4. Access chatbot Settings: After selecting your bot, look for a "Settings" option. This is usually found 
-#    in the sidebar or in the top menu.
-# 5. Find the Bot ID: Within the settings, you should see various details about your bot, including its name, 
-#    platform, and other configurations. Look for a section labeled "Bot ID" or "API Key." This is your 
-#    chatbot's unique identifier in Chatbase.
-# 6. Copy the Bot ID: Once you find the Bot ID, you can copy it for use in your applications or integrations.
 conversation_history_and_other_data = {
     "messages": [],
     "chatbotId": "wGS8ehg-39TolweihWY3w",
@@ -80,20 +39,35 @@ conversation_history_and_other_data = {
     #     "stream": False,
     #     "temperature": 0
     # }
-
+    
 
 # //////////////////////////////////////////////////////////////////////////////////////////
 
 
-# # Setting up the MySQL server:
-# my_sql_relational_database_connection = pymysql.connect(
-#     host="127.0.0.1",
-#     user="root",
-#     password="root",
-#     database="urop_telegram_chatbot_db"
-# )
+# Setting up the Firebase database:
 
-# cursor = my_sql_relational_database_connection.cursor()
+#Here is the link that goes directly to this UROP Telegram Chatbot project's Firebase Realtime database:
+#https://console.firebase.google.com/u/0/project/urop-telegram-chatbot/database/urop-telegram-chatbot-default-rtdb/data
+
+#This if statement, accompanied by the condition with the function 'firebase_admin._apps', is used to prevent
+#any error from creating multiple Firebase apps with the same name. 
+
+#The 'firebase_app = firebase_admin.initialize_app' within this if statement will instantiate a Firebase app 
+#if it is not already created, which is what the 'firebase_admin._apps' does, as an internal variable that 
+#keeps track of the initialized Firebase apps. It is a dictionary-like object that holds information about 
+#the Firebase apps that have been initialized in your Python application.
+if not firebase_admin._apps:
+    # Initialize Firebase
+    credentials_object = firebase_admin.credentials.Certificate("firebase_key.json")
+    firebase_admin.initialize_app(credentials_object, {
+        'databaseURL': 'https://urop-telegram-chatbot-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
+
+# Get a reference to the database
+reference_to_database = db.reference('/')
+
+# Read data from the Realtime Database from Firebase
+print(reference_to_database.get())
 
 
 # //////////////////////////////////////////////////////////////////////////////////////////
@@ -124,18 +98,13 @@ def help_python_function(update, context):
     """
     )
 
-
-
 # /////////////////////////////////////////////////////////////////////////////////////////
-
 
 # The most important function that blends both the Chatbase custom GPT model API and the 'telegram.ext' 
 # Telegram Bot API Python Framework 
 def handle_message_python_function(update, context):
-
     # Appending the user message/prompt to the 'conversation_history_and_other_data' before making the API call
     conversation_history_and_other_data["messages"].append({"content": update.message.text, "role": "user"})
-
 
     # Generating the Chatbase custom GPT response to the user message/prompt when making the API call
     response = requests.post(url, headers=headers, data=json.dumps(conversation_history_and_other_data))
@@ -146,36 +115,26 @@ def handle_message_python_function(update, context):
         update.message.reply_text(f"Chatbase custom GPT model: {json_data['text']}")
         # Appending the generated Chatbase custom GPT response to the 'conversation_history_and_other_data' 
         conversation_history_and_other_data["messages"].append({"content": json_data['text'], "role": "assistant"})
+        print(conversation_history_and_other_data["messages"])
 
 
         # ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-        # # Telegram Chatbot to MySQL server's Relational Database's table things
+        # Telegram Chatbot to Firebase's Realtime Database things
 
-        # # Inserting the student's prompt and telegram chatbot's response as a row in the MySQL server's Relational Database's 
-        # # table using parameterized query.
-        # telegram_chatbot_response = json_data['text']
-        # query = "INSERT INTO telegram_chatbot_history (student_prompt, telegram_chatbot_response) VALUES (%s, %s)"
-        # cursor.execute(query, (update.message.text, telegram_chatbot_response))
-
-        # # Commit the transaction (idk why but this is just needed here to prevent errors)
-        # my_sql_relational_database_connection.commit()
+        #If confirmation submit button is pressed in the Streamlit (Python) web application, the program will 'push' 
+        #basically add this new pieces of user data into the Realtime database in Firebase    
+        reference_to_database.push({"student_prompt" : update.message.text, "telegram_chatbot_response" : json_data['text']})    
 
 
         # ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
+        
 
     # If there is an error when generating the Chatbase custom GPT response for some reason
     else:
         print(f"Error: {json_data}")
         update.message.reply_text(f"Chatbase custom GPT model: Oh no! You encountered an error in the code! The error is: \n'{json_data['message']}'")
-
-    # With reference from code in the Chatbase custom GPT model API documentation:
-    #     if response.status_code == 200:
-    #         print("response:", json_data['text'])
-    #     else:
-    #         print('Error:' + json_data['message'])
 
 
 # //////////////////////////////////////////////////////////////////////////////////////////
