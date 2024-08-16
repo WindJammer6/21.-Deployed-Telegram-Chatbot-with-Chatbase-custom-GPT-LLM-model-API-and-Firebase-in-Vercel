f5c0cc238579e8e54fb4132887c8d4756e5675b3
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2024-08-17 02:25:24 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -46,17 +46,17 @@ if not firebase_admin._apps:
 # Pydantic model for validating Telegram webhook data
 class TelegramWebhook(BaseModel):
     update_id: int
-    message: Optional[dict]
-    edited_message: Optional[dict]
-    channel_post: Optional[dict]
-    edited_channel_post: Optional[dict]
-    inline_query: Optional[dict]
-    chosen_inline_result: Optional[dict]
-    callback_query: Optional[dict]
-    shipping_query: Optional[dict]
-    pre_checkout_query: Optional[dict]
-    poll: Optional[dict]
-    poll_answer: Optional[dict]
+    message: Optional[dict] = None
+    edited_message: Optional[dict] = None
+    channel_post: Optional[dict] = None
+    edited_channel_post: Optional[dict] = None
+    inline_query: Optional[dict] = None
+    chosen_inline_result: Optional[dict] = None
+    callback_query: Optional[dict] = None
+    shipping_query: Optional[dict] = None
+    pre_checkout_query: Optional[dict] = None
+    poll: Optional[dict] = None
+    poll_answer: Optional[dict] = None
 
 
 # Get a reference to the Firebase database
